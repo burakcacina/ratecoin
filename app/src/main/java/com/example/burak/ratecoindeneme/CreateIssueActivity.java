8c2313fba69b876dd3eb5fde2882b56e509f3009
burakcacina <burakccn@hotmail.com>
2018-03-25 19:00:49 +0300

    deneme

--- app/src/main/java/com/example/burak/ratecoindeneme/CreateIssueActivity.java
+++ app/src/main/java/com/example/burak/ratecoindeneme/CreateIssueActivity.java
@@ -46,6 +46,8 @@ public class CreateIssueActivity extends AppCompatActivity {
     int iduser;
 
 
+    //pull
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_createissue);
